fd144e7f1d7c3e3ca6313098c8cb30849af1c95a
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2017-09-21 18:42:09 +0530

    Final website

--- js/slider.js
+++ js/slider.js
@@ -1,50 +1,21 @@
-$('#next').click(function(){ 
-	$('#slider').css({
-		'left':'-=100%'
-	});
-	var element = document.getElementById('slider'),
-    style = window.getComputedStyle(element),
-    left = style.getPropertyValue('left');
-    if(left=='-500px')
-    {
-    	$('#prev').css({
-    		'display': 'block'
-		});
-    }
-    if(left=='-2000px')
-    {
-    	$('#next').css({
-    		'display': 'none'
-    	});
-    }
-});
-$('#prev').click(function(){
-	$('#slider').css({
-		'left':'+=100%'
-	});
-	var element = document.getElementById('slider'),
-    style = window.getComputedStyle(element),
-    left = style.getPropertyValue('left');
-    console.log(left);
-    if(left=='0px')
-    {
-    	$('#prev').css({
-    		'display': 'none'
-    	});
-    }
-});
-$('#about').click(function(){
-    $('#slider').animate({
-        'left':'0%'
-    },500);
-});
-$('#rules').click(function(){
-    $('#slider').animate({
-        'left':'-100%'
-    },500);
-});
-$('#contact').click(function(){
-    $('#slider').animate({
-        'left':'-200%'
-    },500);
+$(document).ready(function(){
+  $("#about").click(function() { 
+    $("#slide1").fadeIn("slow");
+    $("#slide0, #slide2, #slide3, #slide4").fadeOut("fast"); 
+  });
+
+  $("#rules").click(function() { 
+    $("#slide2").fadeIn("slow");
+    $("#slide0, #slide1, #slide3, #slide4").fadeOut("fast"); 
+  });
+
+  $("#contact").click(function() { 
+    $("#slide3").fadeIn("slow");
+    $("#slide0, #slide1, #slide2, #slide4").fadeOut("fast"); 
+  });
+
+  $("#description").click(function() { 
+    $("#slide4").fadeIn("slow");
+    $("#slide0, #slide1, #slide2, #slide3").fadeOut("fast"); 
+  });
 });

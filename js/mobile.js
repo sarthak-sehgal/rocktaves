fec3ec9eacb87fc792a510329c2ff019df2570b6
Laksh Singla <lakshsingla@gmail.com>
2017-09-21 16:53:10 +0530

    Mobile content box added

---
+++ js/mobile.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('#mb-btn-about').on('click',function(){
+        document.getElementById('mb-content-about').style.top="0vh";
+    })
+
+    $('#mb-btn-rules').on('click',function(){
+        document.getElementById('mb-content-rules').style.top="0vh";
+    })
+
+    $('#mb-btn-contact').on('click',function(){
+        document.getElementById('mb-content-contact').style.top="0vh";
+    })
+
+    $('#mb-btn-description').on('click',function(){
+        document.getElementById('mb-content-description').style.top="0vh";
+    })
+
+    
+
+})
+

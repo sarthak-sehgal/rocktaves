49d2a0b6b0fdcee2da32ea1918b9cbcb2b515a5b
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2017-09-24 18:13:03 +0530

    Responsivity increased

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
   $("#about").click(function() { 
-    $("#slide1").fadeIn("slow");
+    $("#slide1, #heading").fadeIn("slow");
     $("#slide0, #slide2, #slide3, #slide4").fadeOut("fast"); 
   });
 
   $("#rules").click(function() { 
-    $("#slide2").fadeIn("slow");
+    $("#slide2, #heading").fadeIn("slow");
     $("#slide0, #slide1, #slide3, #slide4").fadeOut("fast"); 
   });
 
   $("#contact").click(function() { 
-    $("#slide3").fadeIn("slow");
+    $("#slide3, #heading").fadeIn("slow");
     $("#slide0, #slide1, #slide2, #slide4").fadeOut("fast"); 
   });
 
   $("#description").click(function() { 
-    $("#slide4").fadeIn("slow");
+    $("#slide4, #heading").fadeIn("slow");
     $("#slide0, #slide1, #slide2, #slide3").fadeOut("fast"); 
+  });
+  $("#heading").click(function() { 
+    $("#slide0").fadeIn("slow");
+    $("#heading, #slide1, #slide2, #slide3, #slide4").fadeOut("fast"); 
   });
 });
